cd12d396106befb27ee67ac0954e10edc0fa9995
Joel Scott <jscottusf@gmail.com>
2020-02-16 15:11:45 -0500

    added spotify function

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 let keys = require('./keys');
 let axios = require('axios');
-//let spotify = new Spotify(keys.spotify);
+let Spotify = require('node-spotify-api');
+let spotify = new Spotify(keys.spotify);
+let fs = require('fs');
 
 let liriArr = process.argv;
 let liriTask = liriArr[2];
@@ -24,7 +26,16 @@ function getLiriSearch() {
             liriSearch += liriArr[i];
         }
     }
-    
+    else if (liriTask === 'spotify-this-song') {
+        if (liriArr.length > 3) {
+           for (var i = 3; i < liriArr.length; i++) {
+            liriSearch += (liriArr[i] + ' ');
+            }    
+        }
+        else {
+           liriSearch = 'The Sign'; 
+        }
+    }
 }
 
 function searchMovie() {
@@ -32,9 +43,14 @@ function searchMovie() {
     axios.get(omdbApi).then(
         function(response) {
             console.log('================MOVIE=======================');
-            console.log(response.data.Title);
-            console.log("The movie's rating is: " + response.data.imdbRating);
-            console.log("The movie was released in " + response.data.Year);
+            console.log('Title:' + response.data.Title);
+            console.log("Release year: " + response.data.Year);
+            console.log("IMDB rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+            console.log("Nation: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
             console.log('=============================================');
         })
         .catch(function(error) {
@@ -94,6 +110,58 @@ function searchBands() {
         });
 }
 
+function spotifySong() {
+    spotify
+        .search({ 
+            type: 'track', 
+            query: liriSearch,
+            limit: 1
+        })
+        .then(function(response) {
+            for (var key in response) {
+                //console.log(response[key]);
+                console.log('=============SPOTIFY-THIS-SONG===============');
+                //console.log(response[key].items[0]);
+                console.log('Artist Name: ' + response[key].items[0].artists[0].name);
+                console.log('Song Name: ' + response[key].items[0].name);
+                console.log('Preview song link: ' + response[key].items[0].external_urls['spotify']);
+                console.log('Song Album: ' + response[key].items[0].album.name);
+                console.log('=============================================');
+            }
+            //console.log(response.items[key]);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+        //https://api.spotify.com/v1/search?query=all+the+small+things+&type=track&offset=0&limit=20
+        //node liri.js spotify-this-song all the small things
+}
+
+function readRandom() {
+    fs.readFile('random.txt', 'utf8', function(error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
+        }
+        let dataArr = data.split(",");
+        liriTask = dataArr[0];
+        liriSearch = dataArr[1];
+        if (liriTask === 'movie-this') {
+            searchMovie();
+        }
+        else if (liriTask === 'concert-this') {
+            searchBands();
+        }
+        else if (liriTask === 'spotify-this-song') {
+            spotifySong();
+        }
+        else {
+            console.log('Not a recognized command');
+        }
+      });
+}
+
 switch (liriTask) {
     case 'movie-this':
         getLiriSearch();
@@ -103,4 +171,13 @@ switch (liriTask) {
         getLiriSearch();
         searchBands();
         break;
+    case 'spotify-this-song':
+        getLiriSearch();
+        spotifySong();
+        break;
+    case 'do-what-it-says':
+        readRandom();
+        break;
+    default:
+        console.log('Not a recognized command');
 }
